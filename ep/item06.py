053892f30d650b02512fa6c25e4a62e60eb8aa00
tdongsi <dongsi.tuecuong@gmail.com>
2018-08-12 17:38:37 -0700

    Item 6: demo.

--- ep/item06.py
+++ ep/item06.py
@@ -1,10 +1,34 @@
+import json
 
-handle = open('/tmp/random_data.txt', 'w', encoding='utf-8')
-handle.write('success\nand\nnew\nlines')
-handle.close()
+UNDEFINED = object()
 
-handle = open('/tmp/random_data.txt')  # Raise IOError
-try:
-    data = handle.read()   # Raise UnicodeDecodeError
-finally:
-    handle.close()
+
+def divide_json(path):
+    handle = open(path, 'r+')  # IOError
+    try:
+        data = handle.read()    # UnicodeDecodeError
+        op = json.loads(data)   # Value Error
+        value = op['numerator'] / op['denominator']     # ZeroDivisionError
+    except:
+        return UNDEFINED
+    else:
+        op['result'] = value
+        result = json.dumps(op)
+        handle.seek(0)
+        handle.write(result)
+        return value
+    finally:
+        handle.close()
+
+
+def main():
+    temp_path = '/tmp/random_data.json'
+    with open(temp_path, 'w') as handle:
+        handle.write('{"numerator":1.0, "denominator":2}')
+
+    print divide_json(temp_path)
+    pass
+
+
+if __name__ == '__main__':
+    main()
