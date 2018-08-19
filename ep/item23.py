7e730d4382ca37490b43aeca6ae950c606d35cbc
tdongsi <dongsi.tuecuong@gmail.com>
2018-08-18 17:10:34 -0700

    Multiprocessing with subprocess module.

--- ep/item23.py
+++ ep/item23.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import os
 
 # Typical subprocess call to call OS commands
 proc = subprocess.Popen(
@@ -23,6 +24,7 @@ def run_sleep(period):
     proc = subprocess.Popen(['sleep', str(period)])
     return proc
 
+
 start = time.time()
 procs = []
 for _ in range(10):
@@ -35,8 +37,34 @@ for proc in procs:
 end = time.time()
 print('Takes %f seconds' % (end - start))
 
+
 # You can pipe data from Python program to subprocess
 # In this way, you can call other programs to run in parallel with Python process.
-# Run subprocesses on multiple CPUs.
+# Run sub-processes on multiple CPUs.
+
+
+def run_openssl(data):
+    """A computing-intensive method, best for running separately on separate CPUs."""
+    env = os.environ.copy()
+    env['password'] = b'asdf'
+    proc = subprocess.Popen(
+        ['openssl', 'enc', '-des3', '-pass', 'env:password'],
+        env=env,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    proc.stdin.write(data)
+    proc.stdin.flush()
+    return proc
+
+procs = []
+for _ in range(5):
+    data = os.urandom(100)
+    proc = run_openssl(data)
+    procs.append(proc)
+
+for proc in procs:
+    out, _ = proc.communicate()
+    print(out)
 
 
