e30f847c7daaf20c4d82f588a592aee4b2721c1b
tdongsi <dongsi.tuecuong@gmail.com>
2019-02-04 00:18:13 -0800

    Unmodifiable objects in Python

--- ep/item20.py
+++ ep/item20.py
@@ -21,6 +21,23 @@ class LoudResistor(Resistor):
         self._ohms = value
 
 
+class FixedResistor(Resistor):
+    """Immutable object"""
+
+    def __init__(self, ohms):
+        super(FixedResistor, self).__init__(ohms)
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, value):
+        if hasattr(self, '_ohms'):
+            raise AttributeError('Immutable attribute')
+        self._ohms = value
+
+
 def main():
     res = Resistor(1e3)
     print(res.ohms)
@@ -28,6 +45,10 @@ def main():
     print(res.ohms)
 
     res2 = LoudResistor(1e3)
+
+    res3 = FixedResistor(1e3)
+    print(res3.ohms)
+    res3.ohms = 2e3
 
 
 if __name__ == '__main__':
