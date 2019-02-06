49c20f0559af7e679891dc15e91fee2a71ca5407
tdongsi <dongsi.tuecuong@gmail.com>
2019-02-05 23:09:01 -0800

    Private attribute: before.

---
+++ ep/item21.py
@@ -0,0 +1,21 @@
+class ApiClass(object):
+
+    def __init__(self):
+        self._value = 5
+
+    def get(self):
+        return self._value
+
+
+class Child(ApiClass):
+
+    def __init__(self):
+        super(Child, self).__init__()
+
+        # Here, Child class author is not aware of
+        # internal implementation of ApiClass
+        # he accidentally override an internal attribute of ApiClass
+        self._value = 'hello'
+
+a = Child()
+print(a.get())
