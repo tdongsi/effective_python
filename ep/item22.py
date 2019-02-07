4892400504ea4ed261e1ace13bc0e9fa9c95127d
tdongsi <dongsi.tuecuong@gmail.com>
2019-02-07 00:35:19 -0800

    First version of MapReduce.

---
+++ ep/item22.py
@@ -0,0 +1,90 @@
+
+import os
+from threading import Thread
+
+
+class InputData(object):
+    def read(self):
+        raise NotImplementedError
+
+
+class PathInputData(InputData):
+
+    def __init__(self, path):
+        self.path = path
+
+    def read(self):
+        with open(self.path, 'rb') as handle:
+            return handle.read()
+
+
+class Worker(object):
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self):
+        raise NotImplementedError
+
+
+class LineCounter(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count(b'\n')
+        pass
+
+    def reduce(self, other):
+        self.result += other.result
+        pass
+
+
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+
+def create_worker(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCounter(input_data))
+    return workers
+
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    first, rest = workers[0], workers[1:]
+    for other in rest:
+        first.reduce(other)
+    return first.result
+
+
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_worker(inputs)
+    return execute(workers)
+
+
+import random
+from backports.tempfile import TemporaryDirectory
+
+
+def write_test_files(temp_dir):
+    for i in range(100):
+        with open(os.path.join(temp_dir, str(i)), 'w') as handle:
+            handle.write('\n' * random.randint(0, 100))
+
+
+with TemporaryDirectory() as temp_dir:
+    write_test_files(temp_dir)
+    line_count = mapreduce(temp_dir)
+    print line_count
+
