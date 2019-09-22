e6d155fe53839587299ffa391c464ae947713ef9
tdongsi <dongsi.tuecuong@gmail.com>
2019-09-21 21:40:08 -0700

    Patterns

---
+++ ep/patterns.py
@@ -0,0 +1,39 @@
+"""
+From the talk "Lack of Design Patterns in Python".
+
+Example classes, objects, methods for each pattern follow this convetion: PatternName_ObjectName.
+Example: Observer_A, FactoryMethod_Hello
+"""
+# Observer pattern can be achieved with decorator
+def observer(f):
+    def wrapper(self):
+        print("f is called")   # observation
+        return f(self)
+    return wrapper
+
+
+class Observer_A(object):
+    @observer
+    def f(self):
+        return "Hello"
+
+
+observer_a = Observer_A()
+observer_a.f()
+
+
+# Factory Method pattern
+class FactoryMethod_A(object):
+    att = "Hello"
+
+
+class FactoryMethod_B(object):
+    att = "World"
+
+
+factory_method = {
+    "greeting": FactoryMethod_A,
+    "subject": FactoryMethod_B
+}
+
+print factory_method["subject"]().att
